d23ca5d0a9c985069bc968d9df90fb016e7c7a09
Nyagajrr <muchokijnrbenson@gmail.com>
2019-03-29 06:16:08 +0300

    updating the model class

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Institution(models.Model):
     inst_name = models.CharField(max_length = 60)
     inst_pic = models.ImageField(upload_to = 'institutions/')
+    location = models.CharField(max_length = 100)
 
     def __str__(self):
         return self.inst_name
